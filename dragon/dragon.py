768cbaf64ef168af42abe79bcd220780e410fad5
klindyuk <klindyuk@gmail.com>
2022-11-12 13:29:19 +0300

    Занятие 4

--- dragon/dragon.py
+++ dragon/dragon.py
@@ -1,14 +1,17 @@
 import random, time
+playAgain = ''
+while not playAgain.lower().startswith('n'):
 
-secret = random.randint(1, 2)
-print('Вы стоите рядом с двумя пещерами. В одной из них спрятано сокровище, в другой сидит голодный дракон. В какую пещеру вы войдете (1 или 2)?')
-userChoice = input()
-while userChoice != '1' and userChoice != '2':
-    print('Недопустимый ввод. Выберите 1 или 2.')
+    secret = random.randint(1, 2)
+    print('Вы стоите рядом с двумя пещерами. В одной из них спрятано сокровище, в другой сидит голодный дракон. В какую пещеру вы войдете (1 или 2)?')
     userChoice = input()
+    while userChoice != '1' and userChoice != '2':
+        print('Недопустимый ввод. Выберите 1 или 2.')
+        userChoice = input()
 
-time.sleep(2)
-if secret == userChoice:
-    print('Вы нашли сокровище!')
-else:
-    print('К сожалению, вас съели...')
+    time.sleep(2)
+    if secret == int(userChoice):
+        print('Вы нашли сокровище!')
+    else:
+        print('К сожалению, вас съели...')
+    playAgain = input('Сыграть еще? (y/n) ')
