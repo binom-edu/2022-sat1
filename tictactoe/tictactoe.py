33d2cdf228624e6eff7233e8a273deb661dfeaf3
klindyuk <klindyuk@gmail.com>
2023-01-14 13:25:37 +0300

    занятие 11

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -8,7 +8,37 @@ def printBoard(board):
     print(board[1], '|', board[2], '|', board[3])
 
 def getUserMove(board):
-    pass
+    alf = '123456789'
+    while True:
+        s = input('Ваш ход: ').lower()
+        if len(s) != 1:
+            print('Нужно ввести ровно один символ!')
+            continue
+        if not s in alf:
+            print('Требуется ввести цифру')
+            continue
+        s = int(s)
+        if board[s] != ' ':
+            print('Это поле уже занято')
+            continue
+        return s
+
+def makeMove(board, tile, move):
+    board[move] = tile
+
+print('Крестики-нолики')
+print('Используйте цифровую клавиатуру, чтобы сделать ход.')
+
+tiles = ['X', 'O']
+random.shuffle(tiles)
+userTile = tiles[0]
+computerTile = tiles[1]
+print('Вы играете за', userTile)
+ans = input('Поменять (y / n)? ')
+if ans.lower().startswith('y'):
+    userTile, computerTile = computerTile, userTile
+    print('Теперь вы играете за', userTile)
+
 
 board = [' '] * 10
 board[8] = 'O'
@@ -17,3 +47,7 @@ printBoard(board)
 
 turn = random.choice(['user', 'computer'])
 print('Первым ходит:', turn)
+
+userMove = getUserMove(board)
+makeMove(board, userTile, userMove)
+printBoard(board)
