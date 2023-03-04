05b50392ce25a5a320ac3d6c0340bc6a320623b0
klindyuk <klindyuk@gmail.com>
2023-03-04 13:26:45 +0300

    занятие 15

--- locator/locator.py
+++ locator/locator.py
@@ -1,14 +1,31 @@
 import random
+from copy import deepcopy
 
-def printBoard():
+def printBoard(showChests=False):
+    bc = deepcopy(board)
+    if showChests:
+        for i, j in chests:
+            bc[i][j] = '@'
+    print('   ', end='')
     for j in range(WIDTH):
         print(j // 10, end='')
     print()
+    print('   ', end='')
     for j in range(WIDTH):
         print(j % 10, end='')
     print()
+
     for i in range(HEIGHT):
-        print("%2d" % i, '|', *board[i], sep='')
+        print("%2d" % i, '|', *bc[i], '|', i, sep='')
+    
+    print('   ', end='')
+    for j in range(WIDTH):
+        print(j // 10, end='')
+    print()
+    print('   ', end='')
+    for j in range(WIDTH):
+        print(j % 10, end='')
+    print()
 
 def makeBoard():
     board = []
@@ -19,8 +36,25 @@ def makeBoard():
         board.append(line)
     return board
 
+def makeChests():
+    chests = []
+    while len(chests) < MAX_CHESTS:
+        i = random.randrange(0, HEIGHT)
+        j = random.randrange(0, WIDTH)
+        if not (i, j) in chests:
+            chests.append((i, j))
+    return chests
+
+def getUserMove():
+    while True:
+        s = input('Ваш ход (номер строки и столбца через пробел): ').split()
+        
+
 WIDTH = 20
 HEIGHT = 11
+MAX_CHESTS = 3
 EMPTY = '~-'
 board = makeBoard()
-printBoard()
+chests = makeChests()
+printBoard(showChests=True)
+print(chests)
